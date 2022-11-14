98c4d81a821ba203e6286d73d8e0a4031ebaa752
Santhosh Prabhakaran <santhoshprabhakaran02@gmail.com>
2022-11-14 11:05:38 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import CryptoGallery from "./components/crypto-gallery";
 import "./styles/App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CoinDetails from "./components/coin-details";
 
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<CryptoGallery />} />
-          <Route path="/coin-details" element={<CoinDetails />} />
-        </Routes>
-      </BrowserRouter>
+      <CryptoGallery />
     </div>
   );
 }
